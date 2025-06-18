b1334cc7643d01c6349ede7af87815e5323ca3c5
Delaney Gillilan <delaneygillilan@gmail.com>
2025-06-17 19:48:15 -0700

    mergeSignalsOnlyIfMissing-> patchSignalsOnlyIfMissing

--- lib/datastar/consts.rb
+++ lib/datastar/consts.rb
@@ -9,11 +9,11 @@ module Datastar
     # The default duration for retrying SSE on connection reset. This is part of the underlying retry mechanism of SSE.
     DEFAULT_SSE_RETRY_DURATION = 1000
 
-    # Should elements be merged using the ViewTransition API?
+    # Should elements be patched using the ViewTransition API?
     DEFAULT_ELEMENTS_USE_VIEW_TRANSITIONS = false
 
-    # Should a given set of signals merge if they are missing?
-    DEFAULT_MERGE_SIGNALS_ONLY_IF_MISSING = false
+    # Should a given set of signals patch if they are missing?
+    DEFAULT_PATCH_SIGNALS_ONLY_IF_MISSING = false
 
 
     # The mode in which an element is patched into the DOM.
